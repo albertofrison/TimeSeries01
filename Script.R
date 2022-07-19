0e1c928cf082298972b06367cbd68bf0cb8f540b
Alberto Frison <alberto.frison@gmail.com>
2022-07-19 20:27:14 +0200

    1. Minor changes 2. Added June 2022 Actuals data 3. Started Actuals vs Forecasts Analysis

--- Script.R
+++ Script.R
@@ -3,6 +3,7 @@
 # 
 # Source: https://www.acea.auto/figure/passenger-car-registrations-in-europe-since-1990-by-country/
 # Created with ♥ by Alberto Frison - July 2022
+# Thankx to Adam Check for the great YouTube videos: https://www.youtube.com/watch?v=dBNy_A6Zpcc
 ######################################################
 
 ##### 
@@ -16,10 +17,9 @@ library(forecast)
 
 ##### 
 # Section 01 Load the Data
-data <- read.delim ("data/Passengers_Cars_Registrations_Italy_1990_2021.csv", sep = ";")
-data
+data <- read.delim ("data/Passengers_Cars_Registrations_Italy_1990_2021.csv", sep = ";") # Passenger Cars Registrations in Italy from 1990 - 2021
 
-Y <- ts(data[,2], start = 1990, frequency = 12) # declare a new variable Y as Time Series (just the second column)
+Y <- ts(data[,2], start = c(1990,1), frequency = 12) # declare a new variable Y as Time Series (just the second column)
 Y
 
 
@@ -28,51 +28,62 @@ Y
 # Time Plot
 autoplot (Y) + 
   ggtitle ("New Passengers Cars Registrations 1990 - 2021 [Italy]") + 
-  ylab ("Registrations")
-
+  ylab ("Registrations") +
+  xlab ("Year")
 
 # DIFFERENCE DATA TO GET RID OF THE TREND
 DY <- diff(Y) # change of registrations from month to month (feb 1990 - jan 1990)
 autoplot (DY) + 
   ggtitle ("New Passengers Cars Registrations 1990 - 2021 [Italy]") + 
-  ylab ("Registration - Difference")
+  ylab ("Registration - Month over Month Difference") +
+  xlab ("Year")
 
 
 # INVESTIGATING SEASONALITY
-# we got rid of the Trend, let's see if there is a monthly sasonality 
+# we got rid of the Trend, let's see if there is a monthly seasonality 
 
 # one way to look at seasonality
 ggseasonplot (DY) +
-  ggtitle ("Seasonal Plot of New Passengers Cars Registrations 1990 - 2021 [Italy]") + 
-  ylab ("Registration - Difference")
+  ggtitle ("New Passengers Cars Registrations 1990 - 2021 [Italy]") + 
+  ylab ("Registration - Month over Month Difference")
 # there is a clear seasonality in the data with increase of sales in January, as well as March, June and September and a decrease in August
 
 # another way to look at seasonality
-ggsubseriesplot(DY)
+ggsubseriesplot(DY) +
+  ggtitle ("New Passengers Cars Registrations 1990 - 2021 [Italy]") + 
+  ylab ("Registration - Month over Month Seasonality")
 
 
 #####
 # Section 03 The Series has TREND and SEASONALITY
 # Let's try to forecast
 
-
 # 01. Benchmark Methods - SEASONAL NAIVE METHOD: T_y = T_(y-s) + Random Error
 # Remember to use the DIFFERENCE DATA and not the RAW DATA due to the TREND
 
-fit <- snaive(DY)
-print(summary(fit))   # Residual SD 28006.2207
+fit <- snaive(DY)   # Residual SD 28006.2207
+print(summary(fit))   
 checkresiduals(fit)
-#-----------------
 
+# as alternative to checkresiduals 
+res <- residuals (fit)
+Acf(res, main ="ACF of Residuals - Naive Method")
+hist(res, nclass= "FD", main ="Histogram of Residuals - Naive Method")
+
+
+
+#-----------------
 # 02. Exponential Smoothing Model 
 # We can use REGULAR RAW DATA
 
 fit_ets <- ets(Y)         #tries a number of exponential smoothing models and returns the best
 print(summary(fit_ets))   # Residual SD 0.1312
 checkresiduals(fit_ets)
+
+
+
+
 #-----------------
-
-
 # 03. ARIMA
 # Data in ARIMA needs to be STATIONARY: use the Diff data and tell there ise
 
@@ -87,3 +98,30 @@ checkresiduals(fit_arima)
 
 fcst <- forecast(fit_ets,h=24) # we try to forecast two years of data ahead
 autoplot(fcst, include = 60)
+
+
+
+
+#####
+# Check Forecasts against Actuals - Six Months of Data 2022 up to June
+
+# retreiving 2022 h1 registrations data https://www.acea.auto/pc-registrations/passenger-car-registrations-14-0-first-half-of-2022-15-4-in-june/
+italy_2022_h1 <- c(107814, 110869, 119497, 97339, 121299, 127209)
+
+italy_2022_h1 <- ts(italy_2022_h1, start = c(2022,1), end = c(2022,6), frequency = 12)
+
+fcst <- forecast(fit_ets,h=6) # 2022 Jan, Feb, Mar, Apr, May, Jun
+
+accuracy (fcst,x = italy_2022_h1)
+
+plot(fcst)
+lines(italy_2022_h1)
+
+#autoplot(fcst, include = 6)
+tail(fcst)
+
+italy_2022_h1 - fcst
+
+?accuracy
+?forecast
+summary(fcst)
